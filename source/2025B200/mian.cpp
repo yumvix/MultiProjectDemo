6b2339a450b8d00e5c6a601931420234c29a3d3c
yumvix <yumvix@gmail.com>
2025-07-03 23:54:00 +0800

    20250703

--- source/2025B200/mian.cpp
+++ source/2025B200/mian.cpp
@@ -42,10 +42,75 @@ int main() {
 		}
 		cards[value]++;
 	}
+	card[15] = 0;
+	vector<int> seg;
+	for (int i = 3; i < 15; i++)
+	{
+		if(card[i] > 0)
+		{
+			seg.emplace_back(card[i]);
+		}
+		else
+		{
+			vector<int> rst;
+			if (seg.size() < 5)
+			{
+				continue;
+			}
 
+			for (int i = 0; i +4 < seg.size(); i++)
+			{
+				for (int j = i; j< seg.size(); j++)
+				{
+					rst.emplace_back(seg[j]);
+					if (j - i < 4)
+					{
+						continue;
+					}
+					else
+					{
+						for (int k = 0;k < rst.size(); k++)
+						{
+							if (rst[i] == 14)
+							{
+								cout << "A" ;
+							}
+							else if (rst[i] == 15)
+							{
+								cout << "Error";
+									return 0;
+							}
+							else if (rst[i] == 11)
+							{
+								cout << "J" ;
+							}
+							else if (rst[i] == 12)
+							{
+								cout << "Q";
+							}
+							else if (rst[i] == 13)
+							{
+								cout << "K";
+							}
+							else
+							{
+								cout << rst[i] << " ";
+							}
 
-
-
-	std::cout << "Project1";
+							if (k == rst.size()-1)
+							{
+								cout << endl;
+							}
+							else
+							{
+								cout << " ";
+							}
+						}
+					}
+				}
+			}
+			seg.clear();
+		}
+	}
 	return 0;
 }
