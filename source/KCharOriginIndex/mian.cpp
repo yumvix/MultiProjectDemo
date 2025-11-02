4ae5337fdca08469fd2482bb4890bb7fdad2ec43
yumvix <yumvix@gmail.com>
2025-11-02 22:34:23 +0800

    1

--- source/KCharOriginIndex/mian.cpp
+++ source/KCharOriginIndex/mian.cpp
@@ -1,166 +1,119 @@
-锘�#include <iostream>
+#include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
 #include <fstream>
+#include <set>
+#include <map>
+#include <functional>
+#include <iterator>
 using namespace std;
 
-struct Candidate
+
+struct CharAndIndex
 {
-	int strength;
+	char c;
 	int index;
-};
 
-void Match(const vector<Candidate>& input, vector<Candidate>& winer, vector<Candidate>&loser)
-{
-	for (int i = 0; i*2+1 < input.size(); i++)
+	bool operator < (const CharAndIndex& r) const
 	{
-		Candidate candi1 = input[2 * i];
-		Candidate candi2 = input[2 * i + 1];
-		candi1.strength < candi2.strength ? (winer.push_back(candi2), loser.push_back(candi1)) : (winer.push_back(candi1), loser.push_back(candi2));
-	}
-	if (input.size()%2)
-	{
-		winer.push_back(input[input.size() - 1]);
-	}
-}
-
-void MatchFinal(const vector<Candidate>& input, int& first, int& second, int& third)
-{
-	vector<Candidate> winerNew = input;
-	vector<Candidate>loserNew;
-
-	while (winerNew.size()>2)
-	{
-		vector<Candidate> rstWiner;
-		vector<Candidate> rstLoser;
-		Match(winerNew, rstWiner, rstLoser);
-		winerNew = rstWiner;
-		loserNew = rstLoser;
-	}
-	if (winerNew[0].strength >= winerNew[1].strength)
-	{
-		first = winerNew[0].index;
-		second = winerNew[1].index;
-	}
-	else
-	{
-		first = winerNew[1].index;
-		second = winerNew[0].index;
-	}
-	if (loserNew.size() ==1)
-	{
-		third = loserNew[0].index;
-	}
-	else
-	{
-		if (loserNew[0].strength >= loserNew[1].strength)
+		if (this->c == r.c)
 		{
-			third = loserNew[0].index;
+			return this->index < r.index;
 		}
 		else
 		{
-			third = loserNew[1].index;
+			return this->c < r.c;
 		}
 	}
 
-}
-// 方法2：按空格/逗号等分隔符分割文件内容
-std::vector<Candidate> readFileByTokens(const std::string& filePath) {
-	std::vector<Candidate> tokens;
-	std::ifstream file(filePath);
-	if (!file.is_open()) {
-		std::cerr << "Error: Could not open file " << filePath << std::endl;
-		return tokens;
-	}
 
-	std::string line;
-	while (std::getline(file, line)) {
-		std::istringstream iss(line);
-		int token;
-		int index = 0;
-		while (iss >> token) { // 默认按空格分割，可替换为逗号分割
-			Candidate c;
-			c.strength = token;
-			c.index = index;
-			tokens.push_back(c);
-			index++;
-		}
-	}
-	file.close();
-	return tokens;
-}
+};
+
 int main()
 {
-	std::vector<Candidate> input = readFileByTokens("input.txt");
-	int first, second, third;
-	MatchFinal(input, first, second, third);
-	cout << first << " " << second << " " << third;
+	ifstream file;
+	file.open("input.txt");
+
+	string s;
+
+	getline(file, s);
+	stringstream ss(s);
+
+	string input;
+	int index;
+
+	ss >> input;
+	ss >> index;
+
+
+	//map<char, int> sorted;
+	//for (int i = 0; i < input.size(); i++)
+	//{
+
+	//	sorted.insert(std::make_pair(input[i], i));
+	//}
+
+	//for (const auto& c: sorted)
+	//{
+	//	cout << c.first << " " << c.second << endl;
+	//}
+
+	//auto beg = sorted.begin();
+	//for (int i = 0; i < index; i ++)
+	//{
+	//	beg;
+	//}
+
+	//cout << beg->second;
+	function compare = [=](const CharAndIndex& l, const CharAndIndex& r) -> bool {
+		if (l.c == r.c)
+		{
+			return l.index < r.index;
+		}
+		else
+		{
+			return l.c < r.c;
+		}
+		};
+	set<CharAndIndex> sorted;
+	for (int i = 0; i < input.size(); i++)
+	{
+		CharAndIndex temp;
+		temp.c = input[i];
+		temp.index = i;
+		sorted.insert(temp);
+	}
+
+	for (const auto& c : sorted)
+	{
+		cout << c.c << " " << c.index << endl;
+	}
+
+	auto beg = sorted.cbegin();
+
+	for (int i = 0; i < index-1; i++)
+	{
+		 beg++;
+	}
+
+	auto c = beg;
+	for (int i = 0; i < index-1; i++)
+	{
+		if (beg !=sorted.cbegin())
+		{
+			beg--;
+		}
+		else {
+			break;
+		}
+		if (c->c != beg->c)
+		{
+			break;
+		}
+		c = beg;
+	}
+
+	cout << c->index;
 }
 
-
-// 缁欑殑绛旀�堟槸閿欑殑 https://blog.csdn.net/guorui_java/article/details/145382325
-//#include <iostream>
-//#include <vector>
-//
-//class Sportsman {
-//public:
-//	int id;
-//	long strength;
-//	Sportsman(int id, long strength) : id(id), strength(strength) {}
-//};
-
-
-
-//void promotionCompetition(std::vector<Sportsman>& sportsmanList, std::vector<std::vector<Sportsman>>& trophyList) {
-//	std::vector<Sportsman> winList;
-//	std::vector<Sportsman> failList;
-//
-//	for (size_t i = 0; i < sportsmanList.size(); i += 2) {
-//		if (i == sportsmanList.size() - 1) {
-//			winList.push_back(sportsmanList[i]);
-//			continue;
-//		}
-//		Sportsman current = sportsmanList[i];
-//		Sportsman next = sportsmanList[i + 1];
-//
-//		if (next.strength > current.strength) {
-//			winList.push_back(next);
-//			failList.push_back(current);
-//		}
-//		else if (next.strength < current.strength) {
-//			winList.push_back(current);
-//			failList.push_back(next);
-//		}
-//		else {
-//			winList.push_back(current);
-//			failList.push_back(next);
-//		}
-//	}
-//
-//	trophyList.push_back(failList);
-//	trophyList.push_back(winList);
-//}
-//
-//int main() {
-//	int N;
-//	std::cin >> N;
-//	std::vector<Sportsman> sportsmanList;
-//	for (int i = 0; i < N; i++) {
-//		long strength;
-//		std::cin >> strength;
-//		sportsmanList.push_back(Sportsman(i, strength));
-//	}
-//
-//	std::vector<std::vector<Sportsman>> trophyList;
-//	promotionCompetition(sportsmanList, trophyList);
-//
-//	while (trophyList.back().size() > 1) {
-//		promotionCompetition(trophyList[0], trophyList);
-//	}
-//
-//	std::cout << trophyList[0][0].id << " " << trophyList[1][0].id << " " << trophyList[2][0].id << std::endl;
-//
-//	return 0;
-//}
-//
